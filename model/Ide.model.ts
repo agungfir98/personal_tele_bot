657148883359acf184c5344814c86f611e449c89
Agung Firmansyah <agungfir98@gmail.com>
2022-10-04 15:14:00 +0700

    mongodb model

---
+++ model/Ide.model.ts
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+
+const Ide = mongoose.model(
+  "Ideas",
+  new Schema(
+    {
+      name: { type: String, required: true },
+    },
+    { timestamps: true }
+  )
+);
+
+export default Ide;
