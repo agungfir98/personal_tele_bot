d9b9b6c483c36c8f9b2a0920820622e46e059c37
Agung Firmansyah <agungfir98@gmail.com>
2022-10-04 20:51:12 +0700

    check chatid is owner or not

---
+++ utils/isBotOwner.ts
@@ -0,0 +1,10 @@
+import { bot } from "../index.js";
+
+export function checkIsOwner(chatId) {
+  const { OWNER } = process.env;
+
+  if (OWNER !== chatId.toString()) {
+    bot.sendMessage(chatId, "ga blh msk! 😜");
+  }
+  return;
+}
