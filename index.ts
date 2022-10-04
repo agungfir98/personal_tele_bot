c7c056ac544c42aa56067045d08f8d5f2cafba37
Agung Firmansyah <agungfir98@gmail.com>
2022-10-04 15:13:42 +0700

    connecting to mongoDB

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import "dotenv/config";
 import TelegramBot from "node-telegram-bot-api";
-
-import { MessageHandler } from "./utils/switch.js";
-
+import { MessageHandler } from "./handler/messageHandler.js";
+import mongoose from "mongoose";
 const token = process.env.TELE_BOT_TOKEN;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((res) => {
+    console.log("Database berhasil terhubung");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 export const bot = new TelegramBot(token, { polling: true });
 
