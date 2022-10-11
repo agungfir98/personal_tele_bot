aabbd906059f7fd2ea5b94a791eaa66e97681fe4
Agung Firmansyah <agungfir98@gmail.com>
2022-10-11 14:55:08 +0700

    handle beli command

--- handler/index.ts
+++ handler/index.ts
@@ -1,9 +1,11 @@
 import { Message } from "node-telegram-bot-api";
 import { bot } from "../index.js";
 import { byeText, haloText } from "../keyword.js";
+import Balance from "../model/balance.model.js";
+import { HistoryBalance } from "../model/balanceHistory.model.js";
 import { checkIsOwner } from "../utils/isBotOwner.js";
 
-export const MessageHandler = (msg: Message) => {
+export const MessageHandler = async (msg: Message) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
@@ -18,6 +20,50 @@ export const MessageHandler = (msg: Message) => {
       break;
     case haloText.includes(text):
       bot.sendMessage(chatId, `Haloo ${msg.chat.first_name}`);
+      break;
+    // ** handle Beli ============
+    case /beli (.+)/.test(text):
+      const beliArray = text.split(" ");
+
+      const type = beliArray[1];
+      const name = beliArray[2];
+      const value = parseInt(beliArray[3]);
+
+      if (beliArray.length < 4 || beliArray.length > 5) {
+        bot.sendMessage(
+          chatId,
+          `format nggak valid. \nformat yangbener "beli (tipe e.g: fnb, fuel, parking, goods) (nama barang) (duit) (harga)"`
+        );
+      }
+
+      Balance.findOne({ bank: "BNI" })
+        .then((res) => {
+          return new HistoryBalance({
+            title: name,
+            type,
+            value,
+            balance: res.balance,
+            balance_left: res.balance,
+            in_wallet: res.in_wallet,
+            in_wallet_left: res.in_wallet - value,
+            bank: res._id,
+          }).save();
+        })
+        .then(async (res) => {
+          await Balance.findOneAndUpdate(
+            { bank: "BNI" },
+            { $inc: { in_wallet: -value }, $push: { history: res._id } },
+            { new: true }
+          );
+          bot.sendMessage(
+            chatId,
+            `Berhasil menyimpan.\n\nitem: ${res.title}\ntype: ${res.type}\nvalue: Rp.${res.value}\nsisa didompet: Rp.${res.in_wallet_left}\ntanggal: ${res.date}`
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+          bot.sendMessage(chatId, "validasi error ngab, coba cek dompet deh.");
+        });
       break;
     default:
       break;
