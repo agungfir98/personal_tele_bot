9c80a4b574113c197ce211bad8fe359aed64aee3
Agung Firmansyah <agungfir98@gmail.com>
2022-10-11 14:58:10 +0700

    add historyString

--- utils/TextUtils.ts
+++ utils/TextUtils.ts
@@ -1,8 +1,18 @@
-import { Ide } from "../types/type.js";
+import { balanceHistory, Ide } from "../types/type.js";
 
 export const TextCatetan = (res: Ide[]): string => {
   const daftar = res.map((v, i) => {
     return `${i + 1}. ${v.name}\n`;
   });
   return `Daftar catetanmu:\n\n${daftar.join("")}`;
+};
+
+export const historyString = (res: balanceHistory[]): string => {
+  const list = res.map((v, i) => {
+    return `${i + 1}. [${v.type}] ${v.title} Rp.${
+      v.value
+    } Tanggal: ${v.date.toString()}.\n\n`;
+  });
+
+  return `Ingfo:\n\n${list.join("")}`;
 };
