0876099f49f36861afc887bf72dcf16b4e2b0c16
Agung Firmansyah <agungfir98@gmail.com>
2022-10-11 14:56:14 +0700

    type definition for balance and balance history

--- types/type.ts
+++ types/type.ts
@@ -1,3 +1,20 @@
 export interface Ide {
   name: string;
 }
+export interface balance {
+  bank: string;
+  balance: number;
+  in_wallet: number;
+  history: balanceHistory[];
+}
+export interface balanceHistory {
+  bank: any;
+  date: Date;
+  title: string;
+  type: string;
+  value: number;
+  balance: number;
+  in_wallet: number;
+  balance_left: number;
+  in_wallet_left: number;
+}
