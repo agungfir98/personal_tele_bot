5468dd1e868cc5828ba343cb62acf2b811d0c8b1
Agung Firmansyah <agungfir98@gmail.com>
2022-10-11 12:38:53 +0700

    change enum type from narik to tarik tunai

--- model/balanceHistory.model.ts
+++ model/balanceHistory.model.ts
@@ -7,7 +7,10 @@ export const HistoryBalance = mongoose.model<balanceHistory>(
     bank: { type: mongoose.SchemaTypes.ObjectId, ref: "Balance" },
     date: { type: Date, default: () => Date.now() },
     title: { type: String },
-    type: { type: String, enum: ["fnb", "fuel", "goods", "parking", "narik"] },
+    type: {
+      type: String,
+      enum: ["fnb", "fuel", "goods", "parking", "tarik_tunai"],
+    },
     value: { type: Number, min: [0, "yakali minus ngab"] },
     balance: { type: Number, min: [0, "yakali minus ngab"] },
     in_wallet: { type: Number, min: [0, "yakali minus ngab"] },
